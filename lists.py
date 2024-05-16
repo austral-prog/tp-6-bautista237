087c562ae3c35a94dbd9adc2c7d79177ef2fdcb4
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-05-16 18:50:45 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -10,6 +10,7 @@ def add_elements(list_to_add_elements):
    list_to_add_elements.append('yellow')
    print(list_to_add_elements)
 
+
 def is_empty(list_to_check):
     len(list_to_check)
     if len(list_to_check) == 0:
@@ -23,6 +24,7 @@ def check_lists(list_to_compare1, list_to_compare2):
         return True
     else:
         return False
+
 
 def list_of_lists(list_of_lists_to_modify):
         list1 = list_of_lists_to_modify[0][0:2]
